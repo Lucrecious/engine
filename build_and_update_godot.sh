92368c5b91190dedcf551bc51b0056ede4e0d11a
Lucrecious <luca.duran@hotmail.com>
2022-07-01 15:43:14 -0400

    3.x stop

--- build_and_update_godot.sh
+++ build_and_update_godot.sh
@@ -2,12 +2,17 @@
 set -e
 echo "About to start Godot Build"
 cd godot
-scons -j8 platform=x11 custom_modules=../modules
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 	echo "Linux detected."
-	echo "Move to /local/bin/"
+	scons -j8 platform=x11 custom_modules=../modules
+	echo "Move to /usr/local/bin/"
 	sudo cp ./bin/godot.x11.tools.64 /usr/local/bin/godot
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	echo "Mac detected."
+	scons -j8 platform=osx custom_modules=../modules
+	echo "Move to /usr/local/bin/"
+	sudo cp ./bin/godot.osx.tools.64 /usr/local/bin/godot
 else
 	echo "Unknown OS. Will not update."
 fi
